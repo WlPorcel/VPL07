e7d28fef6ceb1fb3bcf2ea36197e10debacb5d30
Ronaldo Gonçalves <100977386+Ronaldo-gsa@users.noreply.github.com>
2022-11-03 21:53:23 -0300

    Add files via upload

--- intruso.cpp
+++ intruso.cpp
@@ -6,16 +6,89 @@
 
 using namespace std;
 
-// Primeira ideia
+// Segunda ideia
+void set_senha_vazada(string vazou){
+  switch(num_entradas){
+    case 2:
+      int valores_A[2];
+      valores_A[0] = senha[0];
+      valores_A[1] = senha[1];
 
-void Intruso::set_senha_vazada(string vazou){
-  map<char,char[2]> mapa;
-  for (int i=0 ; i<16 ; i++){
-    char x = 'A';
-    mapa[x][0] = senha[i];
-    mapa[x][1] = senha[i+2];
+      int valores_B[2];
+      valores_B[0] = senha[0];
+      valores_B[1] = senha[1];
 
+      int valores_C[2];
+      valores_C[0] = senha[0];
+      valores_C[1] = senha[1];
 
+      int valores_D[2];
+      valores_D[0] = senha[0];
+      valores_D[1] = senha[1];
+
+      int valores_E[2];
+      valores_E[0] = senha[0];
+      valores_E[1] = senha[1];
+      break;
+
+    case 3:
+      int valores_A[3];
+        valores_A[0] = senha[0];
+        valores_A[1] = senha[1];
+        valores_A[2] = senha[2];
+  
+        int valores_B[2];
+        valores_B[0] = senha[0];
+        valores_B[1] = senha[1];
+        valores_B[2] = senha[2];
+  
+        int valores_C[3];
+        valores_C[0] = senha[0];
+        valores_C[1] = senha[1];
+        valores_C[2] = senha[2];
+  
+        int valores_D[3];
+        valores_D[0] = senha[0];
+        valores_D[1] = senha[1];
+        valores_D[2] = senha[2];
+  
+        int valores_E[3];
+        valores_E[0] = senha[0];
+        valores_E[1] = senha[1];
+        valores_E[2] = senha[2];
+        break;
+
+      case 4:
+      int valores_A[4];
+        valores_A[0] = senha[0];
+        valores_A[1] = senha[1];
+        valores_A[2] = senha[2];
+        valores_A[3] = senha[3];
+  
+        int valores_B[4];
+        valores_B[0] = senha[0];
+        valores_B[1] = senha[1];
+        valores_B[3] = senha[3];
+        valores_B[4] = senha[4];
+  
+        int valores_C[4];
+        valores_C[0] = senha[0];
+        valores_C[1] = senha[1];
+        valores_C[2] = senha[2];
+        valores_C[3] = senha[3];
+  
+        int valores_D[4];
+        valores_D[0] = senha[0];
+        valores_D[1] = senha[1];
+        valores_D[2] = senha[2];
+        valores_D[3] = senha[3];
+  
+        int valores_E[4];
+        valores_E[0] = senha[0];
+        valores_E[1] = senha[1];
+        valores_E[2] = senha[2];
+        valores_E[3] = senha[3];
+        break;
   }
   
 }
